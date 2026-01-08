0d3c8124a764a5cffe623f7e6e0529e5565eb922
MajorPainTheCactus <thedailytalenewspaper@gmail.com>
2026-01-08 13:55:39 +0000

    Fixed VS2022 deprecation warnings in minidump_generator.cc, Fixed HLSL ternary operator for DXC compiler compatibility

--- renderdoc/3rdparty/breakpad/client/windows/crash_generation/minidump_generator.cc
+++ renderdoc/3rdparty/breakpad/client/windows/crash_generation/minidump_generator.cc
@@ -27,6 +27,8 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+#define _SILENCE_ALL_MS_EXT_DEPRECATION_WARNINGS
+
 #include "breakpad/client/windows/crash_generation/minidump_generator.h"
 
 #include <assert.h>
